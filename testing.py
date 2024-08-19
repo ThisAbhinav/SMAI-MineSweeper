0ac383e4a81f79db45ee5ee4afd5717d285519d2
ThisAbhinav <this.abhinav26@gmail.com>
2024-08-19 15:52:12 +0530

    updateBoard

--- testing.py
+++ testing.py
@@ -1,4 +1,13 @@
 from BoardManager import BoardManager
 
-board = BoardManager(3,3)
-board.displayBoard()
+boardManager = BoardManager(3,3)
+board = boardManager.getBoard()
+for row in board:
+    for cell in row:
+        print(cell, end=" ")
+    print("\n")
+boardManager.updateBoard(0,0)
+for row in board:
+    for cell in row:
+        print(cell, end=" ")
+    print("\n")
