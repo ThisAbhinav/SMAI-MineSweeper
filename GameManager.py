14a5360b09aeab995199999e095a5b232402b61c
jalan-subham <subham.jalan@plaksha.edu.in>
2024-08-21 03:51:44 +0000

    added GameManager

---
+++ GameManager.py
@@ -0,0 +1,12 @@
+from BoardManager import BoardManager, Cell
+from Player import Player
+class GameManager:
+    def __init__(self, boardManager: BoardManager, player: Player):
+        self.boardManager = board
+        self.player = player 
+    def maskBoard(self, board: List[List[Cell]])
+    def play(self):
+        while boardManager.cells_unvisited:
+            realBoard = boardManager.getBoard()
+            maskedBoard = 
+            move = player.makeMove()
--- Player.py
+++
@@ -1,7 +0,0 @@
-from BoardManager import Cell
-
-class Player:
-    def __init__(self, board_size: int) -> None:
-        self.board_size = board_size
-    def make_move(self, board: List[List[Cell]]) -> 
-
