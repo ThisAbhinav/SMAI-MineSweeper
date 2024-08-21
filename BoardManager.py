dfb8a8d26d0e8e123abb8146225caae511700745
jalan-subham <subham.jalan@plaksha.edu.in>
2024-08-21 10:03:27 +0000

    added Game Manager

--- BoardManager.py
+++ BoardManager.py
@@ -1,18 +1,19 @@
 import random
 
 class Cell:
-    def __init__(self, isMine=False) -> None:
+    def __init__(self, isMine : bool = False) -> None:
         self.isMine = isMine
         self.isVisited = False
+        self.adjMineCount = 0
     def __str__(self) -> str:
         if self.isMine:
             return "M"
         elif self.isVisited:
-            return "V"
+            return str(adjMineCount)
         else:
             return "U"
 class BoardManager:
-    def __init__(self, size: int, noOfMines) -> None:
+    def __init__(self, size: int, noOfMines: int) -> None:
         if size*size < noOfMines:
             print("Number of mines are greater than the size of the board")
             exit(1)
@@ -22,7 +23,7 @@ class BoardManager:
         self.board = self.generateRandomBoard(size, noOfMines)
         self.cells_unvisited = size*size - noOfMines
         
-    def generateRandomBoard(self, size, noOfMines) -> list[list[Cell]]:
+    def generateRandomBoard(self, size: int, noOfMines: int) -> list[list[Cell]]:
         tempboard = [[0 for i in range(size)] for j in range(size)]
         minelocations = random.choices(range(size * size),k=noOfMines)
         mines = []
@@ -36,16 +37,29 @@ class BoardManager:
                     tempboard[i][j] = Cell()
         return tempboard
     
-    def getBoard(self):
+    def getBoard(self) -> List[List[Cell]]:
         return self.board
     
-    def updateBoard(self,i,j):
+    def updateBoard(self, (i, j): Tuple[int]) -> None:
         if not self.board[i][j].isMine and not self.board[i][j].isVisited:
             self.board[i][j].isVisited = True
             self.cells_unvisited -=1
+            self.board[i][j].adjMineCount = calculateMineCount((i,j))
         else:
             print("Tried to visit a mine or already visited cell")
             exit(1)
+    def calculateMineCount(self, (i1, j1): Tuple[int]) -> int:
+       mineCount = 0
+       indices = [-1, 0, 1]
+       m = len(self.board)
+       n = len(self.board[0])
+       for i in indices:
+        for j in indices:
+            if i1 + i in range(0, m) and j1 + j in range(0, n):
+                if self.board[i1+i][j1+j].isMine:
+                    mineCount += 1
+        return mineCount
+
         
 
     
