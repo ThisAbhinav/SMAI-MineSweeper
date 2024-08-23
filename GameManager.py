e3f879a2f4e4f8a361c89e4d7f5940d56ea9ac0e
ThisAbhinav <this.abhinav26@gmail.com>
2024-08-23 19:32:39 +0530

    cool

--- GameManager.py
+++ GameManager.py
@@ -1,57 +1,109 @@
 from BoardManager import BoardManager, Cell
+
 from Player import Player
 
 
 class GameManager:
+
     def __init__(self, size: int, noMines: int) -> None:
+
         self.boardManager = BoardManager(size, noMines)
+
         self.player = Player()
 
     def maskCell(self, cell: Cell) -> str:
+
         if cell.isMine:
+
             return "U"
+
         return str(cell)
 
     def displayMaskedBoard(self, board: list[list[str]]) -> None:
-        for i in board:
+
+        print(" ", end=" ")
+
+        print(" ".join([str(i) for i in range(len(board[0]))]))
+
+        for index, i in enumerate(board):
+
+            print(index, end=" ")
+
             for j in i:
+
                 print(j, end=" ")
             print()
 
     def displayRealBoard(self, realBoard: list[list[Cell]]):
-        for i in realBoard:
+
+        print(" ", end=" ")
+
+        print(" ".join([str(i) for i in range(len(realBoard[0]))]))
+
+        for index, i in enumerate(realBoard):
+
+            print(index, end=" ")
+
             for j in i:
+
                 print(j, end=" ")
             print()
 
     def maskBoard(self, board: list[list[Cell]]) -> list[list[str]]:
+
         maskedBoard = [["" for j in range(len(board[0]))] for i in range(len(board))]
+
         for i in range(len(board)):
+
             for j in range(len(board[0])):
+
                 maskedBoard[i][j] = self.maskCell(board[i][j])
+
         return maskedBoard
 
     def isMoveValid(self, t: tuple[int], realBoard: list[list[Cell]]) -> bool:
+
         i, j = t
+
         return not realBoard[i][j].isMine
 
-    def play(self) -> bool:
+    def startHumanPlay(self) -> bool:
+
         correctVisits = 0
+
         totalVisits = self.boardManager.cells_unvisited
+
         while correctVisits < totalVisits:
+
             realBoard = self.boardManager.getBoard()
+
             print("Real Board")
+
             self.displayRealBoard(realBoard)
+
             maskedBoard = self.maskBoard(realBoard)
+
             print("Masked Board")
+
             self.displayMaskedBoard(maskedBoard)
+
             move = self.player.makeMove(maskedBoard)
+
             if self.isMoveValid(move, realBoard):
+
                 correctVisits += 1
+
                 self.boardManager.updateBoard(move)
+
+                print(correctVisits)
+
                 continue
             else:
+
                 print("Player hit a mine. Game over.")
+
                 print("Correctly visited cells:", correctVisits)
+
                 exit(1)
+
         print("All cells visited. Player wins.")
--- testing.py
+++ testing.py
@@ -1,19 +1,24 @@
 from BoardManager import BoardManager
 
+
 # boardManager = BoardManager(3, 3)
+
 # board = boardManager.getBoard()
+
 # for row in board:
+
 #     for cell in row:
+
 #         print(cell, end=" ")
+
 #     print("\n")
-# boardManager.updateBoard(0, 0)
-# for row in board:
-#     for cell in row:
-#         print(cell, end=" ")
-#     print("\n")
+
 
 
 from GameManager import GameManager
 
-gameManager = GameManager(6, 2)
-gameManager.play()
+
+gameManager = GameManager(3, 4)
+
+gameManager.startHumanPlay()
+
